0dcf96ca2d22daae1276f0b3b81fbbd332dc3359
Tobi <toblerus@hotmail.de>
2018-10-01 17:53:50 +0200

    Added status

---
+++ js/status.js
@@ -0,0 +1,14 @@
+        $.ajax({
+            //your server url
+            url: "http://google.com",
+            type: "post",
+            data: "onlinecheck",
+            success: function(){
+                //function if server's online
+                document.getElementById("divtodiplaystatus").innerHTML = "Server is online!";
+            },
+            error:function(){
+                //function if server's offline
+                document.getElementById("divtodiplaystatus").innerHTML = "Server is offline :(!";
+            }
+        });
